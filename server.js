6522d48e0a69b351ad49564298e6805856b551a7
Ben Burgos <ben.a.burgos@gmail.com>
2022-04-08 19:37:30 -0700

    adds initial routes

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const PORT = process.env.PORT;
 const express = require('express');
 const app = express();
 
+const budgetItems = require('./models/budget.js')
 
+app.get('/budgets', (req, res) => {
+    res.send(budgetItems);
+});
+
+app.get('/budgets/:index', (req, res) => {
+    res.send(budgetItems[req.params.index]);
+});
 
 app.listen(PORT, () => {
-    console.log(`You're listening to smooth jazz on port ${PORT}!`)
-})
+    console.log(`You're listening to smooth jazz on port ${PORT}!`);
+});
